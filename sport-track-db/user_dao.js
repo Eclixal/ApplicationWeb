93da0eb5c1047ea2671690fa4c1ec1ec319501d7
griffin568 <messenger133@laposte.net>
2019-10-08 11:40:39 +0200

    test

--- sport-track-db/user_dao.js
+++ sport-track-db/user_dao.js
@@ -1,7 +1,11 @@
 let db = require('./sqlite_connection');
 let UserDAO = function(){
     this.insert = function(values, callback) {
-
+      db.run('INSERT INTO Account VALUES('+values[0]+','+values[1]+','+values[2]+','+values[3]+','+values[4]+','+values[5]+','+values[6]+','+values[7]+')', (error) => {
+        if (error) {
+          console.log("ERROR",error)
+        }
+      })
     };
     this.update = function(key, values, callback) {
 
