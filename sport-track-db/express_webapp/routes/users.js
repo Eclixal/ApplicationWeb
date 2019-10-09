4e06ef5063bf28b24a807f4b9a1722b9878bb9ac
Eclixal <alexandre.jousset@elesia.org>
2019-10-09 07:59:53 +0200

    SEANCE 4

--- sport-track-db/express_webapp/routes/users.js
+++ sport-track-db/express_webapp/routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
+let express = require('express');
+let router = express.Router();
+let user_dao = require('sport-track-db').user_dao;
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    user_dao.findAll(function(rows) {
+      res.render('users', {data:rows});
+  });
 });
-
 module.exports = router;
--- sport-track-db/sqlite_connection.js
+++ sport-track-db/sqlite_connection.js
@@ -1,4 +1,4 @@
 let sqlite3 = require('sqlite3');
-let db = new sqlite3.Database('sport_track.db');
+let db = new sqlite3.Database('./sport_track.db');
 
 module.exports = db;
